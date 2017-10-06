7e06899f1356732cf8d5bd4988e6402cd6969bb0
Nikolai Pawlowski <nikolaip@gmail.com>
2017-10-06 16:28:33 +0000

    Done.

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,5 +1,5 @@
 def starts_with_a_vowel?(word)
-  word.match(/^[aeiou]i/) ? true:false
+  word.match(/^[aeiou]/i) ? true:false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
@@ -11,9 +11,9 @@ def words_five_letters_long(text)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-  text.scan(/\A[A-Z].*[[:punct:]]\z/).length > 0
+  text.match(/\A[A-Z].*[[:punct:]]\z/) ? true:false
 end
 
 def valid_phone_number?(phone)
-  phone.scan(/\(?\d{3}[-\)\s]*\d{3}[-\s]*\d{4}/).length > 0
+  phone.match(/\(?\d{3}[-\)\s]*\d{3}[-\s]*\d{4}/) ? true:false
 end
